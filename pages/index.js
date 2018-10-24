d369439c134cce62ed2b9b6675e10336f238f289
Majisu <merlin.bartel@posteo.de>
2018-10-25 00:28:43 +0200

    Getting contract Balance

--- pages/index.js
+++ pages/index.js
@@ -9,19 +9,22 @@ class Lottery extends Component{
       this.state = {
         manager :'',
         participate_amount :'0.5',
-        message : ''
+        message : '',
+        total_amount : ''
       }
     }
     async componentDidMount(){
         //get the public address of the manager
         const manager = await lottery.methods.manager().call();
         this.setState({manager : manager});
+        const total_amount = await web3.eth.getBalance(lottery.options.address);
+        this.setState({total_amount : total_amount})
     }
 
     render(){
     return (
       <div>
-        <h1> Total Lottery pool is 1000 </h1>
+        <h1> Total Lottery pool is {this.state.total_amount} </h1>
         <form>
           <input placeholder="0.5" />
           <button type="submit">Participate </button>
