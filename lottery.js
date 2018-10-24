d8a5ceddfe489e58f4a84d20ea3174c4fc45561e
Majisu <merlin.bartel@posteo.de>
2018-10-25 00:05:52 +0200

    deploy to rinkeby

--- lottery.js
+++ lottery.js
@@ -1,5 +1,5 @@
 import web3 from './web3.js';
-const adress = '';
+const adress = '0x04C48Ed26901F5382806A7aB1d6bB5cC63C19db2';
 const abi = [{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"participants","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"enterLottery","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
 export default new web3.eth.Contract(abi,adress);
